0a04b3c88cc7203e3c2541660700d44598ce6df5
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-26 20:02:46 -0300

    (REUPLOAD) upload files via Android Dev Tools

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://repo.opencollab.dev/maven-snapshots/")
+        maven("https://repo.opencollab.dev/maven-releases/")
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "AstralConnector"
+include(":app")
