f0f73fae95b92efadd6cd7586fe021bc30692e7d
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-26 20:02:11 -0300

    (REUPLOAD) upload files via Android Dev Tools

---
+++ app/src/main/java/com/astralconnector/ui/Theme.kt
@@ -0,0 +1,26 @@
+package com.astralconnector.ui
+
+import androidx.compose.ui.graphics.Color
+
+object AC {
+    val Primary  = Color(0xFF7C3AED)
+    val Secondary = Color(0xFF5B21B6)
+    val Bg       = Color(0xFF0A0A0F)
+    val Surface  = Color(0xFF111118)
+    val Card     = Color(0xFF1A1A25)
+    val CardAlt  = Color(0xFF1E1E2E)
+    val Border   = Color(0xFF2D2D42)
+    val White    = Color(0xFFE8E8F0)
+    val Muted    = Color(0xFF6B6B8A)
+    val Accent   = Color(0xFF818CF8)
+    val Error    = Color(0xFFEF4444)
+    val Warning  = Color(0xFFF59E0B)
+    val Success  = Color(0xFF10B981)
+    val Cyan     = Color(0xFF06B6D4)
+    val Gold     = Color(0xFFEAB308)
+    val Red      = Color(0xFFEF4444)
+    val Purple   = Color(0xFFA855F7)
+    val Pink     = Color(0xFFEC4899)
+    val GradTop  = Color(0xFF0A0A0F)
+    val GradBot  = Color(0xFF111118)
+}
